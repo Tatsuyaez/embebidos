515ab3605a93f97680452e11c329008144c7e83c
Estudiante IUB <estudiante@itsa.edu.local>
2025-10-24 20:47:44 -0500

    first commit

---
+++ primeraclase.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+//ola
+
+int main()//funcion principal
+{
+
+int numero47 = 14;
+float numero48 = 34;
+double numero3 = 1520;
+char ch1 = 'e';
+
+printf("el valor del numero  2 es: %c\n\n, el valor de numero 1 es:%d\n\n", ch1, numero47);
+printf("el valor del numero  2 es: %d", numero48);//\n permite un salto de linea
+
+
+}
+
